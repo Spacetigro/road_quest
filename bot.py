7c391809ae0444be6b9d4bf550c7a5d1905ca172
Spacetigro <70569505+Spacetigro@users.noreply.github.com>
2025-08-15 11:54:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,8 @@
-import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+import telebot
+from telebot import types
 
-TOKEN = os.getenv("BOT_TOKEN")
+bot= telebot.TeleBot('8221673982:AAH2PwHmXv1Rl4WMkmpZUpPffaRn2lQTtWU')
 
-if not TOKEN:
-    raise ValueError("Токен бота не найден! Установите переменную BOT_TOKEN.")
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.answer("Привет! Бот работает ✅")
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет")
